72b13369dd68906600eade9d8ff8b3d4c90bfb4c
stevecapacity <steve_weiner@outlook.com>
2024-01-15 18:55:19 -0500

    Update lockScreenChange.ps1

--- lockScreenChange.ps1
+++ lockScreenChange.ps1
@@ -1,1 +1,21 @@
+# Script components: do not run as full script
+# These are meant to be added to the intune migration solution
 
+# Change wallpaper after first reboot during middleBoot sequence.
+# Add the below section to 'startMigrate.ps1'
+
+$regPath = "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+$imgPath = "path\ToYour\Migration\Image.jpg"
+
+reg.exe add $regPath /v LockScreenImagePath /t REG_SZ /d $imgPath /f
+reg.exe add $regPath /v LockScreenImageUrl /t REG_SZ /d $imgPath /f
+reg.exe add $regPath /v LockScreenImageStatus /t REG_DWORD /d 1 /f
+
+# The next section will set the Lock screen wallpaper to the corporate image
+# The below should be added to the 'middleBoot.ps1' script
+
+$regPath = "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP"
+$imgPath = "path\ToYour\Corporate\LockScreenImage.jpg"
+
+reg.exe add $regPath /v LockScreenImagePath /t REG_SZ /d $imgPath /f
+reg.exe add $regPath /v LockScreenImageUrl /t REG_SZ /d $imgPath /f
