6817d5314b5e099b3ddbfc0d7b2d40c41b292e53
stevecapacity <steve_weiner@outlook.com>
2024-07-11 11:33:24 -0400

    Create installEntraModule.ps1

---
+++ Misc Intune/installEntraModule.ps1
@@ -0,0 +1,29 @@
+Install-PackageProvider -Name NuGet -Force
+Install-Module -Name PowerShellGet -Force -AllowClobber
+
+Remove-Module PowerShellGet,PackageManagement -Force
+
+Import-Module PowerShellGet -MinimumVersion 2.0 -Force
+Import-PackageProvider PowerShellGet -MinimumVersion 2.0 -Force
+
+$requiredModules = @(
+    "Microsoft.Graph.DirectoryObjects",
+    "Microsoft.Graph.Users",
+    "Microsoft.Graph.Groups",
+    "Microsoft.Graph.Users.Actions",
+    "Microsoft.Graph.Users.Functions",
+    "Microsoft.Graph.Identity.DirectoryManagement",
+    "Microsoft.Graph.Identity.SignIns",
+    "Microsoft.Graph.Governance",
+    "Microsoft.Graph..Applications"
+)
+
+foreach($module in $requiredModules)
+{
+    if(!(Get-Module -Name $module -ListAvailable))
+    {
+        Install-Module -Name $module -Force -AllowClobber
+    }
+}
+
+Install-Module -Name Microsoft.Graph.Entra -Repository PSGallery -Force -AllowPreRelease
